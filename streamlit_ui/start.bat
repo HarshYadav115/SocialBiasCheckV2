a0a8942dbc67c106b4c920d4e96d835dc97eab15
Harsh Yadav <yharsh047@Gmail.com>
2025-06-09 18:51:35 +0530

    Your commit message here

--- streamlit_ui/start.bat
+++ streamlit_ui/start.bat
@@ -1,5 +1,8 @@
 @echo off
-python -m venv .venv
+if not exist .venv (
+    python -m venv .venv
+)
 call .venv\Scripts\activate.bat
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+pip install -r requirements.txt --no-cache-dir
 streamlit run app.py 
